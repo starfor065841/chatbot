3db43b2a7e7053f05df53df4579f71546e8163f9
starfor065841 <player210421@gmail.com>
2018-12-18 00:43:18 +0800

    Fix SETup

--- webhook_handler.py
+++ webhook_handler.py
@@ -93,7 +93,8 @@ def receive_message():
         """Before allowing people to message your bot, Facebook has implemented a verify token
         that confirms all requests that your bot receives came from Facebook.""" 
         token_sent = request.args.get("hub.verify_token")
-        return verify_fb_token(token_sent)
+        mode = request.GET.get("hub.mode")
+        return verify_fb_token(token_sent, mode)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
         # get whatever message a user sent the bot
@@ -136,10 +137,10 @@ def receive_message():
     return "Message Processed"
 
 
-def verify_fb_token(token_sent):
+def verify_fb_token(token_sent, mode):
     #take token sent by facebook and verify it matches the verify token you sent
     #if they match, allow the request, else return an error 
-    if token_sent == VERIFY_TOKEN:
+    if token_sent == VERIFY_TOKEN and mode == "subscribe":
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
@@ -186,4 +187,4 @@ def send_vid(recipient_id, url):
 '''
 
 if __name__ == "__main__":
-    webhook_handler.run(host='0.0.0.0', debug=True)
+    webhook_handler.run(host='0.0.0.0', port = 12345, debug=True)
